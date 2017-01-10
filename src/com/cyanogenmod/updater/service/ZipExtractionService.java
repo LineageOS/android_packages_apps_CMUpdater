04df3c1285c85e0602f4061f7354f0cd2f52b37b
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-10 13:55:48 -0600

    [WIP] CMUpdater: support A/B style OTA updates
    TODO:
    - Pop up when ready to install letting user know about it being backgrounded
      and they can continue using their device and that they will be notified
      when ready to reboot
    - Ongoing notification about the update taking place
      - Block user from starting a new update while one is occuring
      - Support for cancelling an in-progress update (via app and notification?)
    - Notify when completed or if error occurs and prompt reboot
    - Backuptool-like support for gapps and whatnot
    
    Change-Id: Icfdd1e7128d5981e6c9dbd9fd5dc16891e3168f5

---
+++ src/com/cyanogenmod/updater/service/ZipExtractionService.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.cyanogenmod.updater.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.cyanogenmod.updater.utils.Utils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ZipExtractionService extends IntentService {
+
+    private static final String TAG = "ZipExtractionService";
+
+    // broadcast actions
+    public static final String ACTION_EXTRACT_FINISHED = "com.cyanogenmod.cmupdater.action.ACTION_EXTRACT_FINISHED";
+    public static final String ACTION_EXTRACT_ERRORED = "com.cyanogenmod.cmupdater.action.ACTION_EXTRACT_ERRORED";
+    public static final String EXTRA_ZIP_NAME = "zip_name";
+
+    public ZipExtractionService() {
+        super("ZipExtractionService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String filename = intent.getStringExtra(EXTRA_ZIP_NAME);
+        extractZip(filename);
+    }
+
+    private void extractZip(String filename) {
+        String outputdir = Utils.makeUpdateFolder(getApplicationContext()).getPath() + "/unzipped/";
+        Intent intent = new Intent(ACTION_EXTRACT_FINISHED);
+
+        try {
+            ZipInputStream zin = new ZipInputStream(new FileInputStream(filename));
+            ZipEntry entry;
+            File d;
+
+            d = new File(outputdir);
+            if (d.exists()) {
+                Utils.deleteDir(d);
+            }
+
+            d.mkdirs();
+
+            while ((entry = zin.getNextEntry()) != null) {
+                String name = entry.getName();
+
+                if (entry.isDirectory()) {
+                    d = new File(outputdir, name);
+                    if (!d.exists()) {
+                        d.mkdirs();
+                    }
+                    continue;
+                }
+
+                int s = name.lastIndexOf(File.separatorChar);
+                String dir = s == -1 ? null : name.substring(0, s);
+                if (dir != null) {
+                    d = new File(outputdir, dir);
+                    if (!d.exists()) {
+                        d.mkdirs();
+                    }
+                }
+
+                byte[] buffer = new byte[4096];
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(outputdir, name)));
+                int count = -1;
+                while ((count = zin.read(buffer)) != -1) {
+                    out.write(buffer, 0, count);
+                }
+                out.close();
+            }
+
+            zin.close();
+            intent.setAction(ACTION_EXTRACT_FINISHED);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to extract zip", e);
+            intent.setAction(ACTION_EXTRACT_ERRORED);
+        }
+
+        sendBroadcast(intent);
+    }
+}
