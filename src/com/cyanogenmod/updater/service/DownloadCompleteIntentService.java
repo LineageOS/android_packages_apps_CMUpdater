c4654dc82758c950ad541fc1b5ebd728a3162c97
syphyr <syphyr@gmail.com>
2017-01-28 04:20:50 +0100

    Updater: close file channels in onHandleIntent
    This prevents the output stream (destFile) from getting corrupted.
    
    If the file channels are not properly closed, a condition can
    occur that corrupts the output stream.
    
    Fixes: https://review.lineageos.org/#/c/154714/
    
    Change-Id: I95caed05ff5c2ae5758c8426a767d8e58383afbc

--- src/com/cyanogenmod/updater/service/DownloadCompleteIntentService.java
+++ src/com/cyanogenmod/updater/service/DownloadCompleteIntentService.java
@@ -80,13 +80,18 @@ public class DownloadCompleteIntentService extends IntentService {
 
             try {
                 FileOutputStream outStream = new FileOutputStream(destFile);
-
                 ParcelFileDescriptor file = mDm.openDownloadedFile(id);
                 FileInputStream inStream = new FileInputStream(file.getFileDescriptor());
-
                 FileChannel inChannel = inStream.getChannel();
                 FileChannel outChannel = outStream.getChannel();
-                inChannel.transferTo(0, inChannel.size(), outChannel);
+                try {
+                    inChannel.transferTo(0, inChannel.size(), outChannel);
+                } finally {
+                    inChannel.close();
+                    outChannel.close();
+                    inStream.close();
+                    outStream.close();
+                }
             } catch (IOException e) {
                 displayErrorResult(updateIntent, R.string.unable_to_download_file);
                 return;
