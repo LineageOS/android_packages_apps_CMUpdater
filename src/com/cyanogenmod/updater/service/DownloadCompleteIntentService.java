59455f7e35aabad953ed982277b401e272e67d0a
Gabriele M <moto.falcon.git@gmail.com>
2017-03-25 01:48:24 +0100

    Delete partially copied files
    We might have some incomplete file if FileChannel.transferTo() fails,
    delete it so that it won't show up as downloaded.
    
    Change-Id: I2a406a5aeca8c22aa2be3c3a5dda6c1c9dd13891

--- src/com/cyanogenmod/updater/service/DownloadCompleteIntentService.java
+++ src/com/cyanogenmod/updater/service/DownloadCompleteIntentService.java
@@ -82,6 +82,9 @@ public class DownloadCompleteIntentService extends IntentService {
             } catch (IOException e) {
                 Log.e(TAG, "Copy of download failed", e);
                 displayErrorResult(updateIntent, R.string.unable_to_download_file);
+                if (destFile.exists()) {
+                    destFile.delete();
+                }
                 return;
             } finally {
                 mDm.remove(id);
